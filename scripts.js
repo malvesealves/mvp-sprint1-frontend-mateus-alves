d35152b42399ba431aee921c795ff79511f6022e
Mateus Alves e Alves <mouts-mateus.alves@ailos.coop.br>
2023-07-14 23:11:53 -0300

    Commit final

--- scripts.js
+++ scripts.js
@@ -1,41 +1,5 @@
 /*
   --------------------------------------------------------------------------------------
-  Chamada da função para carregamento inicial dos tipos de pet
-  --------------------------------------------------------------------------------------
-*/
-getTypes()
-
-/*
-  --------------------------------------------------------------------------------------
-  Função para obter a lista existente do servidor via requisição GET
-  --------------------------------------------------------------------------------------
-*/
-const getTypes = async () => {
-  let url = 'http://127.0.0.1:5000/tipos';
-  fetch(url, {
-    method: 'get',
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      options = data.tipos;
-
-      var select = document.getElementById("newType");
-      for(var i = 0; i < options.length; i++) {
-        var opt = options[i];
-        var el = document.createElement("option");
-        el.textContent = opt;
-        el.value = opt;
-        select.appendChild(el);
-    }
-    })
-    .catch((error) => {
-      console.error('Error:', error);
-    });  
-  
-}
-
-/*
-  --------------------------------------------------------------------------------------
   Função para obter a lista existente do servidor via requisição GET
   --------------------------------------------------------------------------------------
 */
@@ -46,7 +10,7 @@ const getList = async () => {
   })
     .then((response) => response.json())
     .then((data) => {
-      data.pets.forEach(item => insertList(item.nome, item.quantidade, item.valor))
+      data.pets.forEach(item => insertList(item.nome, item.idade, item.responsavel))
     })
     .catch((error) => {
       console.error('Error:', error);
@@ -105,7 +69,6 @@ const insertButton = (parent) => {
 */
 const removeElement = () => {
   let close = document.getElementsByClassName("close");
-  // var table = document.getElementById('myTable');
   let i;
   for (i = 0; i < close.length; i++) {
     close[i].onclick = function () {
@@ -143,17 +106,19 @@ const deleteItem = (item) => {
   --------------------------------------------------------------------------------------
 */
 const newItem = () => {
-  let inputProduct = document.getElementById("newInput").value;
-  let inputQuantity = document.getElementById("newQuantity").value;
-  let inputPrice = document.getElementById("newPrice").value;
+  let inputName = document.getElementById("newName").value;
+  let inputIdade = document.getElementById("newAge").value;
+  let inputResponsavel = document.getElementById("newResponsible").value;
 
-  if (inputProduct === '') {
-    alert("Escreva o nome de um item!");
-  } else if (isNaN(inputQuantity) || isNaN(inputPrice)) {
-    alert("Quantidade e valor precisam ser números!");
+  if (inputName === '') {
+    alert("Escreva o nome de um cachorro!");
+  } else if (isNaN(inputIdade)) {
+    alert("Idade precisa ser números!");
+  } else if (inputResponsavel === '') {
+    alert("Escreve o responsável do cachorro!");
   } else {
-    insertList(inputProduct, inputQuantity, inputPrice)
-    postItem(inputProduct, inputQuantity, inputPrice)
+    insertList(inputName, inputIdade, inputResponsavel)
+    postItem(inputName, inputIdade, inputResponsavel)
     alert("Item adicionado!")
   }
 }
@@ -163,8 +128,8 @@ const newItem = () => {
   Função para inserir items na lista apresentada
   --------------------------------------------------------------------------------------
 */
-const insertList = (nameProduct, quantity, price) => {
-  var item = [nameProduct, quantity, price]
+const insertList = (nameProduct, idade, responsavel) => {
+  var item = [nameProduct, idade, responsavel]
   var table = document.getElementById('myTable');
   var row = table.insertRow();
 
@@ -172,10 +137,11 @@ const insertList = (nameProduct, quantity, price) => {
     var cel = row.insertCell(i);
     cel.textContent = item[i];
   }
+
   insertButton(row.insertCell(-1))
-  document.getElementById("newInput").value = "";
-  document.getElementById("newQuantity").value = "";
-  document.getElementById("newPrice").value = "";
+  document.getElementById("newName").value = "";
+  document.getElementById("newAge").value = "";
+  document.getElementById("newResponsible").value = "";
 
   removeElement()
 }
